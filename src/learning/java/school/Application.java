0d7d376e01e85d80e90c560ee30733f4a656ef61
Israel Félix <iFelix@outlook.es>
2020-09-10 01:36:43 -0500

    Captura asistencia e informa después (reto)

--- src/learning/java/school/Application.java
+++ src/learning/java/school/Application.java
@@ -7,12 +7,13 @@ public class Application {
     public static void main(String[] args) {
 
         Student []student = new Student[]{
-                /* Inicializando el arreglo */
-                new Student("Isaias","1"),
-                new Student("Edgar","2"),
-                new Student("Alain","3"),
-                new Student("Caleb","4")
+                // Inicializando el arreglo
+                new Student("Alaín","1"),
+                new Student("Israel","2"),
+                new Student("Jocelyn","3"),
+                new Student("Hugo","4")
         };
+        int []asistencia = new int [student.length];
 
         Scanner scanner = new Scanner(System.in);
 
@@ -25,7 +26,21 @@ public class Application {
             System.out.print("Asistió?: ");
             String response = scanner.next("[YNyn]");
 
-            System.out.println(student[i].getName() + (response.equalsIgnoreCase("Y")? " si" : " no"));
+            // System.out.println(student[i].getName() + (response.equalsIgnoreCase("Y")? " si" : " no"));
+
+            if (response.equalsIgnoreCase("Y")){
+                asistencia[i]=1;
+            }else{
+                asistencia[i]=0;
+            }
+        }
+        for(int i = 0; i < student.length ; i++) {
+            System.out.print(student[i].getName() + " - ");
+            if(asistencia[i] == 1) {
+                System.out.println("Sí asistió");
+            }else{
+                System.out.println("No asistió");
+            }
         }
     }
 }
